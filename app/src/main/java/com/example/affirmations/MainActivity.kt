5bd8bf34325eef6c26cecc88cb3d7c7e49a73ada
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-02-07 08:14:43 -0300

    New Layout with Grid
    Signed-off-by: Fabricio Valadares <fabricio.valadares@gmail.com>

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.affirmations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.DataSource
 import com.example.affirmations.databinding.ActivityMainBinding
@@ -11,16 +12,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-//        binding.textView.text =
-//            """Amount of Affirmations: """ + DataSource().loadAffirmations().size
-        val myDataSet = DataSource().loadAffirmations()
 
+        val myDataSet = DataSource().loadAffirmations()
         val recyclerView = binding.recyclerView
 
+        val layoutManager = GridLayoutManager(
+            applicationContext, 2
+        )
+
         recyclerView.adapter = ItemAdapter(this, myDataSet)
+        recyclerView.layoutManager = layoutManager
         recyclerView.setHasFixedSize(true)
     }
 }
